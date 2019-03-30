955ad7cb925dac5d6a2a51d42b6d0c019e9a191b
marbuser <marbuser@gmail.com>
2019-03-31 01:55:03 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <test-component></test-component>
+  </div>
+</template>
+
+<script>
+import testComponent from '@/components/TestComponent.vue';
+
+export default {
+  name: 'app',
+  components: {
+    testComponent,
+  },
+};
+</script>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>This is a test component.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+};
+</script>
